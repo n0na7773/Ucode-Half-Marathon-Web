061365d2c37574d3bfff59a1dd81ebdc3649a59f
Ilya Razumeyko <irazumeyko@c1r7p8.khpi.ucode-connect.study>
2021-03-08 15:57:31 +0200

    pomoyka

--- Web-Sprint02/t04/js/script.js
+++ Web-Sprint02/t04/js/script.js
@@ -1,6 +1,12 @@
 function checkDivision(beginRange, endRange) {
+    if (!beginRange) {
+        beginRange = 1;
+    }
+    if (!endRange) {
+        endRange = 100;
+    }
     let res = "\n";
-    for (let i = beginRange; i <= endRange; i++) {
+    for (let i = beginRange; i - 1 != endRange; i++) {
         if (i % 2 == 0) {
             res += `${i} is even`;
             if (i % 3 == 0) {
@@ -29,11 +35,13 @@ function checkDivision(beginRange, endRange) {
     }
     console.log(res);
 }
-var num = prompt("Type the beginning and end of a range from 1 to 100 with space between. Example: 25 50.");
+var num = prompt("Type the beginning and end of a range from 1 to 100 with space between. Example: 25 50.\nYou also can sent one of them empty(but leave space as divisor) or fully empty row and receive default values.");
 var arr = num.split(" ");
-while (((!(arr[0] > 0) || !(arr[0] <= 100)) || (!(arr[1] > 0) || !(arr[1] <= 100))) || !(arr[0] < arr[1])) {
+console.log(arr[0].length);
+console.log(arr[1].length);
+console.log(arr[0].lenght == arr[1].lenght);
+while ((!(arr[0] < arr[1]) && arr[0].lenght == arr[1].lenght)) {
     num = prompt("Type the beginning and end of a range from 1 to 100 with space between. Example: 25 50.");
     arr = num.split(" ");
 }
 checkDivision(arr[0], arr[1]);
-
