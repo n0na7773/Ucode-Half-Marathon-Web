59552ac5a6677c8a75774ae3d10804397a06a372
Ilya Razumeyko <irazumeyko@c1r5p5.khpi.ucode-connect.study>
2021-03-31 17:36:24 +0300

    t00

--- Web-Sprint07/t07/index.php
+++ Web-Sprint07/t07/index.php
@@ -1,23 +1,15 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <title>SERVER</title>
-</head>
-
-<body>
-    <?php
-    echo $_SERVER["PHP_SELF"]." <br>";          
-    echo $_SERVER["argv"]." <br>";              
-    echo $_SERVER["SERVER_ADDR"]." <br>";       
-    echo $_SERVER["HTTP_HOST"]." <br>";         
-    echo $_SERVER["SERVER_PROTOCOL"]." <br>";   
-    echo $_SERVER["QUERY_STRING"]." <br>";      
-    echo $_SERVER["HTTP_USER_AGENT"]." <br>";   
-    echo $_SERVER["REMOTE_ADDR"]." <br>";       
-    echo $_SERVER["REQUEST_URI"]." <br>";       
-    ?>
-</body>
-
-</html>
+<?php
+class Product {
+    private $name, $kcal_per_portion;
+    public function __construct($name, $kcal_per_portion) {
+        $this->name = $name;
+        $this->kcal_per_portion = $kcal_per_portion;
+    }
+    public function getName() {
+        return $this->name;
+    }
+    public function getKcal() {
+        return $this->kcal_per_portion;
+    }
+}
+?>
