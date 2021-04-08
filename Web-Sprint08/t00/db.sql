0c1e474fbbe732b8e10d8f557cf297eb4676c5b1
Ilya <37002484+n0na7773@users.noreply.github.com>
2021-04-08 18:49:36 +0300

    Delete Web-Sprint08 directory

--- Web-Sprint08/t00/db.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE ucode_web;
-CREATE USER 'irazumeyko'@'localhost' IDENTIFIED WITH mysql_native_password BY 'securepass';
-GRANT ALL PRIVILEGES ON ucode_web . * TO 'irazumeyko'@'localhost';
-FLUSH PRIVILEGES;
--- Web-Sprint08/t05/find_me.sql
+++
@@ -1,6 +0,0 @@
-USE ucode_web;
-
-SELECT output.NAME, output.ID FROM (
-    SELECT heroes.ID, heroes.NAME FROM heroes WHERE heroes.NAME 
-        LIKE BINARY '%a%' AND heroes.RACE != 'human' AND heroes.CLASS_ROLE IN ('tankman','healer')
-) AS output JOIN teams ON output.ID = teams.HERO_ID GROUP BY output.ID HAVING COUNT(teams.HERO_ID) > 1 ORDER BY output.ID ASC LIMIT 1;
